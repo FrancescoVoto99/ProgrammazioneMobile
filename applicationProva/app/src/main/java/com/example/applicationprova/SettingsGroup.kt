7afde2f9a142d8a5487226d1a422581851b42deb
FrancescoVoto99 <S1087880@studenti.univpm.it>
2021-08-26 17:47:13 +0200

    start settings group

---
+++ applicationProva/app/src/main/java/com/example/applicationprova/SettingsGroup.kt
@@ -0,0 +1,11 @@
+package com.example.applicationprova
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsGroup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_group)
+    }
+}
