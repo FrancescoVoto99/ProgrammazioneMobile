1aefd13c6616864da1d57fe8eea9be750620f2fa
LucaRanucci <86157572+LucaRanucci@users.noreply.github.com>
2021-08-31 18:34:16 +0200

    Added checkbox on productlist

--- applicationProva/app/src/main/java/com/example/applicationprova/ListofProductAdapter.kt
+++ applicationProva/app/src/main/java/com/example/applicationprova/ListofProductAdapter.kt
@@ -2,20 +2,19 @@ package com.example.applicationprova
 
 import android.content.Intent
 import android.util.Log
+import android.util.SparseBooleanArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 
 class ListofProductAdapter (val data: List<String>,val data2: List<String>, val idGroup: String):
     RecyclerView.Adapter<ListofProductAdapter.MyViewHolder>() {
+    var checkBoxStateArray = SparseBooleanArray()
 
-    class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
-        val textView = row.findViewById<TextView>(R.id.item)
-        val textView2 = row.findViewById<TextView>(R.id.idproduct)
-    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
             MyViewHolder {
         val layout = LayoutInflater.from(parent.context)
@@ -34,8 +33,48 @@ class ListofProductAdapter (val data: List<String>,val data2: List<String>, val 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.textView.text = data.get(position).toString()
         holder.textView2.text = data2.get(position).toString()
+        if(!checkBoxStateArray.get(position,false))
+        {//checkbox unchecked.
+            holder.checkBox.isChecked = false
+        }
+        else
+        {//checkbox checked
+            holder.checkBox.isChecked = true
+        }
+        //gets position from data object
+        var data_position = data[position]
+
         Log.d("ListofGroupAdapter",data.get(position).toString())
     }
 
     override fun getItemCount(): Int = data.size
+
+    inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+        val textView = row.findViewById<TextView>(R.id.item)
+        val textView2 = row.findViewById<TextView>(R.id.idproduct)
+        val checkBox = row.findViewById<CheckBox>(R.id.checkBox)
+
+        init
+        {//called after the constructor.
+
+            checkBox.setOnClickListener {
+
+                if(!checkBoxStateArray.get(adapterPosition,false))
+                {//checkbox checked
+                    checkBox.isChecked = true
+                    //stores checkbox states and position
+                    checkBoxStateArray.put(adapterPosition,true)
+                }
+                else
+                {//checkbox unchecked
+                    checkBox.isChecked = false
+                    //stores checkbox states and position.
+                    checkBoxStateArray.put(adapterPosition,false)
+                }
+
+            }
+        }
+    }
+
+
 }
---
+++ applicationProva/app/src/main/java/com/example/applicationprova/ProdottoArr.kt
@@ -0,0 +1,8 @@
+package com.example.applicationprova
+
+abstract class ProdottoArr {
+    abstract val name: String
+    abstract val id: String
+    abstract val check: Boolean
+
+}
