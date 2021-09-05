54a2c05929b0a8de00d30e2a604bc4409f0b6d69
FrancescoVoto99 <S1087880@studenti.univpm.it>
2021-09-05 17:28:42 +0200

    listener statistic

---
+++ applicationProva/app/src/main/java/com/example/applicationprova/ListOfProductCategory.kt
@@ -0,0 +1,68 @@
+package com.example.applicationprova
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.applicationprova.databinding.ActivityListOfProductsBinding
+import com.example.applicationprova.databinding.ActivityListOfProductsBoughtBinding
+import com.example.progetto.Prodotto
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
+class ListOfProductCategory : AppCompatActivity() {
+
+    lateinit var database: FirebaseDatabase
+    lateinit var myRef: DatabaseReference
+    lateinit var searchproducts: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val list = mutableListOf<String>()
+        val list2 = mutableListOf<Prodotto>()
+        super.onCreate(savedInstanceState)
+        //data binding al posto del classico inflate
+        val binding: ActivityListOfProductsBoughtBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_list_of_product_category
+        )
+        //setContentView(R.layout.activity_list_of_groups)
+
+
+        database = FirebaseDatabase.getInstance("https://prova-14ff5-default-rtdb.europe-west1.firebasedatabase.app/")
+        searchproducts = database.getReference("gruppi")
+
+
+        val rv: RecyclerView = binding.listaProdottiComprati
+        rv.layoutManager = LinearLayoutManager(this)
+
+        val extras = intent.extras
+        if (extras != null) {
+            val value = extras.getString("key")
+            val categoria = extras.getString("categoria")
+
+            searchproducts.child(value.toString()).child("prodotti").get().addOnSuccessListener {
+                for (postSnapshot in it.children) {
+                    if ((postSnapshot.child("buy").getValue().toString()).equals("1") &&
+                            (postSnapshot.child("categoria").getValue().toString()).equals(categoria)) {
+                        list.add(postSnapshot.child("nome").getValue().toString())
+                        val product: Prodotto = it.getValue<Prodotto>() as Prodotto
+                        list2.add(product)
+                        rv.adapter = ListOfProductBoughtAdapter(list2)
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
--- applicationProva/app/src/main/java/com/example/applicationprova/Statistic.kt
+++ applicationProva/app/src/main/java/com/example/applicationprova/Statistic.kt
@@ -1,17 +1,22 @@
 package com.example.applicationprova
 
+import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.firebase.database.FirebaseDatabase
+
 
 class Statistic : AppCompatActivity() {
 
@@ -24,18 +29,20 @@ class Statistic : AppCompatActivity() {
             FirebaseDatabase.getInstance("https://prova-14ff5-default-rtdb.europe-west1.firebasedatabase.app/")
         val searchproducts = database.getReference("gruppi")
         val categorie = listOf<String>("Cibo", "Bagno", "Casa", "Salute", "Divertimento", "Altro")
-        val quantita = mutableListOf<Int>(0,0,0,0,0,0)
+        val quantita = mutableListOf<Int>(0, 0, 0, 0, 0, 0)
 
 
         var piechart: PieChart = findViewById(R.id.piechart)
         val entries: ArrayList<PieEntry> = ArrayList()
         val colors: ArrayList<Int> = ArrayList()
 
+        val intent = Intent(this, ListOfProductCategory::class.java)
+
         val extras = intent.extras
         if (extras == null) {
             //val value = extras.getString("key")
 
-            searchproducts.child("-MiXSn0c4-lP0nyiX6Wu").child("prodotti").get().addOnSuccessListener {
+            searchproducts.child("-MignrLG-d51pOLQA7Ob").child("prodotti").get().addOnSuccessListener {
                 for (item in 0..(categorie.size-1)) {
                     for (postSnapshot in it.children) {
                         if ((postSnapshot.child("buy").getValue()
@@ -45,17 +52,35 @@ class Statistic : AppCompatActivity() {
                             quantita[item] =
                                     (quantita[item] + (postSnapshot.child("quantita").value
                                     .toString().toInt()))
-                            print("proviamo "+quantita[item])
+                            print("proviamo " + quantita[item])
 
                         }
                     }}
+
+
+
+                piechart.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
+                    override fun onValueSelected(e: Entry, h: Highlight) {
+                        intent.putExtra("key", "-MignrLG-d51pOLQA7Ob")
+                        intent.putExtra("categoria", e.data.toString())
+                        startActivity(intent)
+                        val y = e.y
+                        Log.d("prova",y.toString()+"  "+ e.data)
+                    }
+
+                    override fun onNothingSelected() {}
+                })
+
                     for (item in 0..(categorie.size-1)) {
                         if(quantita[item]!=0){
                             val norma=quantita[item] / quantita.sum().toFloat()
-                        entries.add(PieEntry(norma,categorie[item]))
+                        entries.add(PieEntry(norma, categorie[item],categorie[item]))
+
                     }}
+
+
                 insertcolors(colors)
-                createPieChart(entries,piechart,colors)
+                createPieChart(entries, piechart, colors)
                 createLegend(piechart)
 
 
@@ -81,7 +106,7 @@ class Statistic : AppCompatActivity() {
         }
     }
 
-    fun insertcolors(colors:ArrayList<Int>){
+    fun insertcolors(colors: ArrayList<Int>){
 
         for (color in ColorTemplate.MATERIAL_COLORS) {
             colors.add(color)
@@ -93,7 +118,7 @@ class Statistic : AppCompatActivity() {
         }
 
     }
-    fun createPieChart(entries: ArrayList<PieEntry>,piechart: PieChart,colors: ArrayList<Int>){
+    fun createPieChart(entries: ArrayList<PieEntry>, piechart: PieChart, colors: ArrayList<Int>){
         val dataSet: PieDataSet = PieDataSet(entries, "Prodotti Acquistati")
         dataSet.setColors(colors)
 
